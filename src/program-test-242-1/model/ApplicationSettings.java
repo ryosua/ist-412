60797347c833a919a34742aa9f0207431bb80389
Dan Servich <dservich@gmail.com>
2015-09-09 09:02:01 -0400

    Adding the settings to be saved to a text file in case the user wants to do it manually

--- src/program-test-242-1/model/ApplicationSettings.java
+++ src/program-test-242-1/model/ApplicationSettings.java
@@ -2,11 +2,15 @@ package model;
 
 
 import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class ApplicationSettings {
 
     private File sourceFileDirectory = null;
     private File testCaseDirectory = null;
+	private File javaVersionDirectory = null;
+	private String settingsFileName = "Settings.txt";
 
     public File getSourceFileDirectory() {
         return sourceFileDirectory;
@@ -18,9 +22,48 @@ public class ApplicationSettings {
 
     public void setSourceFileDirectory(File directory) {
         sourceFileDirectory = directory;
+		writeDataToSettingsFile();
     }
 
     public void setTestCaseDirectory(File directory) {
         testCaseDirectory = directory;
+		writeDataToSettingsFile();
     }
+	
+	public void writeDataToSettingsFile(){
+		PrintWriter out = new PrintWriter(settingsFileName);
+		
+		if(sourceFileDirectory != null){
+			out.println("Source File Directory: " + sourceFileDirectory.toString());
+		}
+		
+		if(testCaseDirectory != null){
+			out.println("Test Case Directory: " + testCaseDirectory.toString());
+		}
+		
+		if(javaVersionDirectory != null){
+			out.println("Java Version Directory: " + javaVersionDirectory.toString());
+		}
+		
+		out.close();
+	}
+	
+	public void readDataFromSettingsFile(){
+		
+		Scanner inFile = new Scanner(new File(settingsFileName));
+		
+		while(inFile.hasNextLine()){
+			String setting = inFile.nextLine();
+			
+			if(setting.startsWith("Source File Directory: ")){
+				sourceFileDirectory = new File(setting.substring("Source File Directory: ".length()));
+			}
+			else if(setting.startsWith("Test Case Directory: ")){
+				testCaseDirectory = new File(setting.substring("Test Case Directory: ".length()));
+			}
+			else if(setting.startsWith("Java Version Directory: ")){
+				javaVersionDirectory = new File(setting.substring("Java Version Directory: ".length()));
+			}
+		}
+	}
 }
