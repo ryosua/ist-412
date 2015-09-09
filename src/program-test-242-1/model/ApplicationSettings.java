5950fd8a371c11c9b346df08f374701d46c94ea4
Ryan Yosua <rcy5017@psu.edu>
2015-09-09 13:52:43 -0400

    Added try/catch so the program will compile.

--- src/program-test-242-1/model/ApplicationSettings.java
+++ src/program-test-242-1/model/ApplicationSettings.java
@@ -1,7 +1,7 @@
 package model;
 
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -9,8 +9,8 @@ public class ApplicationSettings {
 
     private File sourceFileDirectory = null;
     private File testCaseDirectory = null;
-	private File javaVersionDirectory = null;
-	private String settingsFileName = "Settings.txt";
+    private File javaVersionDirectory = null;
+    private String settingsFileName = "Settings.txt";
 
     public File getSourceFileDirectory() {
         return sourceFileDirectory;
@@ -31,39 +31,45 @@ public class ApplicationSettings {
     }
 	
 	public void writeDataToSettingsFile(){
-		PrintWriter out = new PrintWriter(settingsFileName);
-		
-		if(sourceFileDirectory != null){
-			out.println("Source File Directory: " + sourceFileDirectory.toString());
-		}
-		
-		if(testCaseDirectory != null){
-			out.println("Test Case Directory: " + testCaseDirectory.toString());
-		}
-		
-		if(javaVersionDirectory != null){
-			out.println("Java Version Directory: " + javaVersionDirectory.toString());
-		}
-		
-		out.close();
+            try (PrintWriter out = new PrintWriter(settingsFileName)) {
+                if(sourceFileDirectory != null){
+                    out.println("Source File Directory: " + sourceFileDirectory.toString());
+                }
+
+                if(testCaseDirectory != null){
+                    out.println("Test Case Directory: " + testCaseDirectory.toString());
+                }
+
+                if(javaVersionDirectory != null){
+                    out.println("Java Version Directory: " + javaVersionDirectory.toString());
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
 	}
 	
 	public void readDataFromSettingsFile(){
-		
-		Scanner inFile = new Scanner(new File(settingsFileName));
-		
-		while(inFile.hasNextLine()){
-			String setting = inFile.nextLine();
-			
-			if(setting.startsWith("Source File Directory: ")){
-				sourceFileDirectory = new File(setting.substring("Source File Directory: ".length()));
-			}
-			else if(setting.startsWith("Test Case Directory: ")){
-				testCaseDirectory = new File(setting.substring("Test Case Directory: ".length()));
-			}
-			else if(setting.startsWith("Java Version Directory: ")){
-				javaVersionDirectory = new File(setting.substring("Java Version Directory: ".length()));
-			}
-		}
+            Scanner inFile = null;
+            try {
+                inFile = new Scanner(new File(settingsFileName));
+
+                while(inFile.hasNextLine()){
+                    String setting = inFile.nextLine();
+
+                    if(setting.startsWith("Source File Directory: ")){
+                        sourceFileDirectory = new File(setting.substring("Source File Directory: ".length()));
+                    }
+                    else if(setting.startsWith("Test Case Directory: ")){
+                        testCaseDirectory = new File(setting.substring("Test Case Directory: ".length()));
+                    }
+                    else if(setting.startsWith("Java Version Directory: ")){
+                        javaVersionDirectory = new File(setting.substring("Java Version Directory: ".length()));
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                inFile.close();
+            }
 	}
 }
