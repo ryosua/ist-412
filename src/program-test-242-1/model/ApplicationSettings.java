56626c133edbedaff0c448c88a2f987ea58fcce3
Ryan Yosua <rcy5017@psu.edu>
2015-09-09 16:06:57 -0400

    Fixed indentation.

--- src/program-test-242-1/model/ApplicationSettings.java
+++ src/program-test-242-1/model/ApplicationSettings.java
@@ -30,46 +30,46 @@ public class ApplicationSettings {
 		writeDataToSettingsFile();
     }
 	
-	public void writeDataToSettingsFile(){
-            try (PrintWriter out = new PrintWriter(settingsFileName)) {
-                if(sourceFileDirectory != null){
-                    out.println("Source File Directory: " + sourceFileDirectory.toString());
-                }
-
-                if(testCaseDirectory != null){
-                    out.println("Test Case Directory: " + testCaseDirectory.toString());
-                }
-
-                if(javaVersionDirectory != null){
-                    out.println("Java Version Directory: " + javaVersionDirectory.toString());
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+    public void writeDataToSettingsFile(){
+        try (PrintWriter out = new PrintWriter(settingsFileName)) {
+            if(sourceFileDirectory != null){
+                out.println("Source File Directory: " + sourceFileDirectory.toString());
             }
-	}
-	
-	public void readDataFromSettingsFile(){
-            Scanner inFile = null;
-            try {
-                inFile = new Scanner(new File(settingsFileName));
 
-                while(inFile.hasNextLine()){
-                    String setting = inFile.nextLine();
-
-                    if(setting.startsWith("Source File Directory: ")){
-                        sourceFileDirectory = new File(setting.substring("Source File Directory: ".length()));
-                    }
-                    else if(setting.startsWith("Test Case Directory: ")){
-                        testCaseDirectory = new File(setting.substring("Test Case Directory: ".length()));
-                    }
-                    else if(setting.startsWith("Java Version Directory: ")){
-                        javaVersionDirectory = new File(setting.substring("Java Version Directory: ".length()));
-                    }
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } finally {
-                inFile.close();
+            if(testCaseDirectory != null){
+                out.println("Test Case Directory: " + testCaseDirectory.toString());
             }
-	}
+
+            if(javaVersionDirectory != null){
+                out.println("Java Version Directory: " + javaVersionDirectory.toString());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readDataFromSettingsFile(){
+        Scanner inFile = null;
+        try {
+            inFile = new Scanner(new File(settingsFileName));
+
+            while(inFile.hasNextLine()){
+                String setting = inFile.nextLine();
+
+                if(setting.startsWith("Source File Directory: ")){
+                    sourceFileDirectory = new File(setting.substring("Source File Directory: ".length()));
+                }
+                else if(setting.startsWith("Test Case Directory: ")){
+                    testCaseDirectory = new File(setting.substring("Test Case Directory: ".length()));
+                }
+                else if(setting.startsWith("Java Version Directory: ")){
+                    javaVersionDirectory = new File(setting.substring("Java Version Directory: ".length()));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            inFile.close();
+        }
+    }
 }
