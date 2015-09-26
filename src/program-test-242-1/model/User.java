9ae1f833e2b4c3b7fe4621654e6489da14815dce
Ryan Yosua <rcy5017@psu.edu>
2015-09-26 19:13:02 -0400

    Register users.

--- src/program-test-242-1/model/User.java
+++ src/program-test-242-1/model/User.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Scanner;
+
 public class User {
     private String password;
     private String username;
@@ -7,6 +9,15 @@ public class User {
     public User(String password, String username) {
         this.password = password;
         this.username = username;
+    }
+    
+    public static User fromString(String userString) {
+        Scanner in = new Scanner(userString);
+        
+        String username = in.next();
+        String password = in.next();
+        
+        return new User(username, password);
     }
     
     public String getUsername() {
@@ -23,5 +34,10 @@ public class User {
     
     public void setUsername(String username) {
        this.username = username; 
+    }
+    
+    @Override
+    public String toString() {
+        return username + " " + password;
     }
 }
--- src/program-test-242-1/view/RegisterPanel.java
+++ src/program-test-242-1/view/RegisterPanel.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.Main;
+import controller.listener.RegisterUserListener;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import javax.swing.JButton;
@@ -12,17 +13,28 @@ public class RegisterPanel extends JPanel {
     
     private final Main main;
     
+    private JTextField passwordField;
+    private JTextField usernameField;
+    
     public RegisterPanel(Main main) {
         this.main = main;
 
         createAndAddComponents();
     }
+    
+    public String getPassword() {
+        return passwordField.getText();
+    }
+    
+    public String getUserName() {
+        return usernameField.getText();
+    }
 
     private void createAndAddComponents() {
-       final GridBagLayout layout = new GridBagLayout();
-       setLayout(layout);
-       
-       final JLabel usernameLabel = new JLabel("Username:");
+        final GridBagLayout layout = new GridBagLayout();
+        setLayout(layout);
+
+        final JLabel usernameLabel = new JLabel("Username:");
         final GridBagConstraints usernameLabelC = new GridBagConstraints();
         usernameLabelC.fill = GridBagConstraints.HORIZONTAL;
         usernameLabelC.gridx = 0;
@@ -33,25 +45,26 @@ public class RegisterPanel extends JPanel {
         passwordLabelC.fill = GridBagConstraints.HORIZONTAL;
         passwordLabelC.gridx = 0;
         passwordLabelC.gridy = 2;
-        
-        final JTextField usernameField = new JTextField();
+
+        usernameField = new JTextField();
         final GridBagConstraints usernameFieldC = new GridBagConstraints();
         usernameFieldC.fill = GridBagConstraints.HORIZONTAL;
         usernameFieldC.gridx = 0;
         usernameFieldC.gridy = 1;
-        
-        final JTextField passwordField = new JTextField();
+
+        passwordField = new JTextField();
         final GridBagConstraints passwordFieldC = new GridBagConstraints();
         passwordFieldC.fill = GridBagConstraints.HORIZONTAL;
         passwordFieldC.gridx = 0;
         passwordFieldC.gridy = 3;
-        
+
         final JButton registerButton = new JButton("Register");
         final GridBagConstraints registerButtonC = new GridBagConstraints();
         registerButtonC.fill = GridBagConstraints.HORIZONTAL;
         registerButtonC.gridx = 0;
         registerButtonC.gridy = 4;
-        
+        registerButton.addActionListener(new RegisterUserListener(main, this));
+
         add(usernameLabel, usernameLabelC);
         add(passwordLabel, passwordLabelC);
         add(usernameField, usernameFieldC);
