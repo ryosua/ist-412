7ab28f4208a1f7387712b84a4b58e378a9dbf80d
Dan Servich <dservich@gmail.com>
2015-10-05 09:26:52 -0400

    added Output stuff. Still needs some display/control work. But it displays.

---
+++ src/program-test-242-1/controller/listener/OutputCheckBoxListener.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.AbstractButton;
+import model.ApplicationSettings;
+
+/**
+ *
+ * @author dos5385
+ */
+public class OutputCheckBoxListener implements ActionListener{
+
+    private ApplicationSettings settings;
+    public OutputCheckBoxListener(ApplicationSettings settings){
+        this.settings = settings;
+        
+    }
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        AbstractButton abstractButton = (AbstractButton) e.getSource();
+        boolean selected = abstractButton.getModel().isSelected();
+        //String newLabel = (selected ? "A" : "B");
+        //abstractButton.setText(newLabel);
+        if(selected){
+            System.out.println("Check Box clicked. Value is: " + selected);
+            settings.setDisplayOutputCheck(Boolean.TRUE.toString());
+        }else{
+            System.out.println("Check Box unclicked. Value is: " + selected);
+            settings.setDisplayOutputCheck(Boolean.FALSE.toString());
+        }
+        
+    }
+    
+}
---
+++ src/program-test-242-1/view/OutputFrame.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author dos5385
+ */
+public class OutputFrame extends JFrame{
+    private  OutputPanel output;
+    private String results;
+    public OutputFrame(String results){
+        this.results = results;
+        initFrame();
+        
+    }
+    
+    public void initFrame(){
+        
+        this.setSize(500, 500);
+        //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        
+        output = new OutputPanel(results);
+        this.add(output);
+        
+        
+        this.setVisible(true);
+    }
+}
