3ab10cb0a7acb0f4e5792ffc3c39370564d22635
Ryan Yosua <rcy5017@psu.edu>
2015-10-06 18:45:05 -0400

    Added unit testing. Still needs tests.

---
+++ src/program-test-242-1/ProgramJUnitTest.java
@@ -0,0 +1,31 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ProgramJUnitTest
+{
+    @BeforeClass
+    public static void setupTests() {
+        System.out.println("Starting Tests");
+    }
+    
+    @Before
+    public void setupTest() {
+       
+    }
+    
+    // Ryan's Tests *****************************************************
+    @Test
+    public void exampleTest() {
+        fail("This is an example test that should fail.");
+    }
+    
+    @After
+    public void teardownTest() {
+        System.out.println("Test Finished");
+    }
+
+    @AfterClass
+    public static void teardownTests() {
+        System.out.println("All Tests Finished");
+    }
+}
