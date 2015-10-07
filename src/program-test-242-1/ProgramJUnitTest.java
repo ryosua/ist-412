64cd460474ecc7876a87597721adf7a8ea034ac8
Ryan Yosua <rcy5017@psu.edu>
2015-10-07 12:22:32 -0400

    Added tests for the file filter class.

--- src/program-test-242-1/ProgramJUnitTest.java
+++ src/program-test-242-1/ProgramJUnitTest.java
@@ -1,21 +1,35 @@
+import controller.FileFilter;
+import controller.Main;
+import java.io.File;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ProgramJUnitTest
 {
+    private Main main;
+    
     @BeforeClass
     public static void setupTests() {
-        System.out.println("Starting Tests");
+        System.out.println("Starting Tests");   
     }
     
     @Before
     public void setupTest() {
-       
+        main = new Main();
     }
    
     @Test
-    public void exampleTest() {
-        fail("This is an example test that should fail.");
+    public void fileFilterAcceptsJavaFiles() {
+        File javaFileDir = new File(main.getSettings().getTestCaseDirectory().getAbsolutePath());
+        FileFilter fileFilter = new FileFilter();
+        assertTrue(fileFilter.accept(javaFileDir, "SomeClass.java"));
+    }
+    
+    @Test
+    public void fileFilterRejectsNonJavaFiles() {
+        File javaFileDir = new File(main.getSettings().getTestCaseDirectory().getAbsolutePath());
+        FileFilter fileFilter = new FileFilter();
+        assertFalse(fileFilter.accept(javaFileDir, "SomeClass.txt"));
     }
     
     @After
