7515cc3abfe3e8ede942669048efeadb9a11f336
Ryan Yosua <rcy5017@psu.edu>
2015-10-07 12:22:15 -0400

    Formatting, separated construction of frame from constructor so that tests and cli don't open the gui.

--- src/program-test-242-1/controller/Main.java
+++ src/program-test-242-1/controller/Main.java
@@ -7,46 +7,50 @@ import view.InputPanel;
 
 public class Main {
 
-    private final Frame f;
     private final FileController fileController;
     private final ApplicationSettings settings;
     private final UserController userController;
-   
+
+    private Frame f;
+
     public Main() {
         // Changing the order may cause null pointers.
         settings = new ApplicationSettings();
         settings.readDataFromSettingsFile();
         userController = new UserController();
-        
-        f = new Frame(this);
         fileController = new FileController(this);
     }
 
     public static void main(String[] args) {
         /*Can fix display issues with mac.
-        try
-        {
-            UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
-        }catch (Exception e)
-        {
-            e.printStackTrace();
-        }*/
-        
+         try
+         {
+         UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
+         }catch (Exception e)
+         {
+         e.printStackTrace();
+         }*/
+
         final Main mainController = new Main();
+        mainController.openFrame();
     }
-    
+
+    public void openFrame() {
+        f = new Frame(this);
+    }
+
     public FileController getFileController() {
         return fileController;
     }
-    
+
     public JFrame getFrame() {
         return f;
     }
-    
+
     public ApplicationSettings getSettings() {
         return settings;
     }
-    
+
     public UserController getUserController() {
         return userController;
     }
