c0f6739dff2eb192000d283e12acb7a68514eb37
Ryan Yosua <rcy5017@psu.edu>
2015-10-14 22:57:13 -0400

    SingleTesterTest

---
+++ src/program-test-242-1/SingleTesterTest.java
@@ -0,0 +1,59 @@
+import controller.Main;
+import controller.SingleTester;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SingleTesterTest
+{
+    private Main main;
+    
+    @BeforeClass
+    public static void setupTests() {
+        System.out.println("Starting Tests");   
+    }
+    
+    @Before
+    public void setupTest() {
+        main = new Main();
+    }
+   
+    @Test
+    public void theOutputContainsNoExceptions() {
+        // Run the program in single test mode.
+        final SingleTester singleTest = new SingleTester(main.getSettings());
+        singleTest.run();
+        
+        // Check to see if the output contains any exceptions.
+        Scanner in = null;
+        boolean error = false;
+        try {
+            in = new Scanner(main.getSettings().getOutputFileDirectory());
+            
+            while(in.hasNext()) {
+                if (in.nextLine().toLowerCase().contains("exception")
+                    || in.nextLine().toLowerCase().contains("error")) {
+                    error = true;
+                }
+            }
+           
+        } catch (FileNotFoundException ex) {
+            error = true;
+        } finally {
+            in.close();
+        }
+        
+        assertFalse(error);
+    }
+    
+    @After
+    public void teardownTest() {
+        System.out.println("Test Finished");
+    }
+
+    @AfterClass
+    public static void teardownTests() {
+        System.out.println("All Tests Finished");
+    }
+}
