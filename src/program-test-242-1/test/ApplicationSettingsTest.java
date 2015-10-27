1ac311bd35635e003871f2f4c41cb2f33db77973
Ryan Yosua <rcy5017@psu.edu>
2015-10-26 23:16:02 -0400

    Started the ApplicationSettingsTest.
    #41

---
+++ src/program-test-242-1/test/ApplicationSettingsTest.java
@@ -0,0 +1,81 @@
+package test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ApplicationSettingsTest {
+    @BeforeClass
+    public static void setupTests() {
+        System.out.println("Starting Tests");   
+    }
+    
+    @Before
+    public void setupTest() {
+        
+    }
+    
+    @Test
+    public void byDefaultSettingsAreEmptyFiles() {
+        
+    }
+    
+    @Test
+    public void configFileReadAndWrite() {
+        
+    }
+    
+    @Test
+    public void javaVersionDirectoryReadAndWrite() {
+        
+    }
+    
+    @Test
+    public void outputFileDirectoryReadAndWrite() {
+        
+    }
+    
+    @Test
+    public void rootDirectoryReadAndWrite() {
+        
+    }
+    
+    @Test
+    public void runModeReadAndWrite() {
+        
+    }
+    
+    @Test
+    public void sourceFileDirectoryReadAndWrite() {
+        
+    }
+    
+    @Test
+    public void testCaseDirectoryReadAndWrite() {
+        
+    }
+    
+    @Test
+    public void displayOutputCheckReadAndWrite() {
+        
+    }
+    
+    @Test
+    public void testToString() {
+        
+    }
+    
+    @After
+    public void teardownTest() {
+        System.out.println("Test Finished");
+    }
+
+    @AfterClass
+    public static void teardownTests() {
+        System.out.println("All Tests Finished");
+    }
+}
