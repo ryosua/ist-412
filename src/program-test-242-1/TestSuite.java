f1e849c00235f5ad85cb22e334997047f145aa18
Ryan Yosua <rcy5017@psu.edu>
2015-10-27 11:37:36 -0400

    Got further on the application settings test. Need to test using a different settings file.

--- src/program-test-242-1/TestSuite.java
+++ src/program-test-242-1/TestSuite.java
@@ -4,10 +4,12 @@ import test.FileFilterTest;
 import test.BatchTesterTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import test.ApplicationSettingsTest;
 import test.MainTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+    //working on this //ApplicationSettingsTest.class,
     BatchTesterTest.class,
     FileFilterTest.class,
     MainTest.class,
--- src/program-test-242-1/test/ApplicationSettingsTest.java
+++ src/program-test-242-1/test/ApplicationSettingsTest.java
@@ -1,5 +1,9 @@
 package test;
 
+import controller.ApplicationSettingsController;
+import controller.Main;
+import java.io.File;
+import model.ApplicationSettings;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertFalse;
@@ -9,6 +13,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ApplicationSettingsTest {
+    private Main main;
+    
     @BeforeClass
     public static void setupTests() {
         System.out.println("Starting Tests");   
@@ -16,17 +22,26 @@ public class ApplicationSettingsTest {
     
     @Before
     public void setupTest() {
-        
+        main = new Main();
     }
     
     @Test
     public void byDefaultSettingsAreEmptyFiles() {
-        
+        File emptyFile = new File("");
+        ApplicationSettings settings = new ApplicationSettings();
+        assertTrue(settings.getConfigFile().equals(emptyFile));
+        //ect
     }
     
     @Test
     public void configFileReadAndWrite() {
-        
+        ApplicationSettings settings = new ApplicationSettings();
+        File testFile = new File("testConfig.txt");
+        settings.setConfigFile(testFile);
+        ApplicationSettingsController.writeDataToSettingsFile(settings);
+        ApplicationSettings newSettings = new ApplicationSettings();
+        ApplicationSettingsController.readDataFromSettingsFile(settings);
+        assertTrue(settings.getConfigFile().equals(newSettings.getConfigFile()));
     }
     
     @Test
