a798c057f939a925afa5ca1a06c720318d09941b
Shane Eckenrode <sue2@psu.edu>
2015-11-01 21:11:37 -0500

    Add initial text reading to text panel area. Added tutorial text for first 2 lines in tutorialText.txt

--- src/program-test-242-1/view/HelpTextPanel.java
+++ src/program-test-242-1/view/HelpTextPanel.java
@@ -2,19 +2,25 @@ package view;
 
 import controller.Main;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import model.ApplicationSettings;
 
 public class HelpTextPanel extends JPanel{
-    private JTextArea tutorialText;
+    private ArrayList<String> tutorialText;
+    private JTextArea tutorialTextArea;
     private File tutorialTextFile;
     private Main main;
     private int rows;
     private int columns;
+    private int currentText;
     
     public HelpTextPanel(){
         main = new Main();
+        tutorialText = new ArrayList<String>();
         
         //Setup the panel
         setupPanel();
@@ -22,17 +28,21 @@ public class HelpTextPanel extends JPanel{
         //Setup the textfile pathing
         setupTutorialFile(main.getSettings());
         
+        //Add text for steps
+        readTextStepsFromFile(tutorialTextFile);
+        
         //Read the textfile and write into the panel.
-        writeTextToPanel(tutorialTextFile);
+        addTextToPanel(currentText);
     }
 
     private void setupPanel(){
+        currentText = 0;
         rows = 30;
         columns = 30;
-        tutorialText = new JTextArea(rows, columns);
-        tutorialText.setEditable(false);
+        tutorialTextArea = new JTextArea(rows, columns);
+        tutorialTextArea.setEditable(false);
         
-        add(tutorialText);
+        add(tutorialTextArea);
     }
     
     //Store path for tutorial textfile in settings?
@@ -40,7 +50,21 @@ public class HelpTextPanel extends JPanel{
         tutorialTextFile = new File(settings.getTutorialTextDirectory().getAbsolutePath());
     }
     
-    private void writeTextToPanel(File text){
-        
+    private void readTextStepsFromFile(File text){
+        try{
+            Scanner in = new Scanner(text);
+            while(in.hasNextLine()){
+                if(!in.nextLine().equals("")){
+                    System.out.println(in.nextLine());
+                    tutorialText.add(in.nextLine());
+                }
+            }
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void addTextToPanel(int text){
+        tutorialTextArea.setText(tutorialText.get(text));
     }
 }
