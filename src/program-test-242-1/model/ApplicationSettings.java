4d5e9762cdbb71b533f3bfa1d57be5c9fc145e28
Ryan Yosua <rcy5017@psu.edu>
2015-11-03 11:32:34 -0500

    Removed run mode from settings classes + formatting.

--- src/program-test-242-1/model/ApplicationSettings.java
+++ src/program-test-242-1/model/ApplicationSettings.java
@@ -4,40 +4,35 @@ import controller.ApplicationSettingsController;
 import java.io.File;
 
 public class ApplicationSettings {
+
     public final static String SINGLE_MODE = "Single Mode";
     public final static String BATCH_MODE = "Batch Mode";
-   
+
     private final File settingsFile = new File("Settings.txt");
-    
+
     private File configFile = new File(""); // Just single for now.
     private File javaVersionDirectory = new File("");
     private File outputFileDirectory = new File("");
     private File rootDirectory = new File("");
     private File sourceFileDirectory = new File("");
     private File testCaseDirectory = new File("");
-    
-    private String runMode = SINGLE_MODE;
+
     private boolean displayOutputCheck = true;
-            
-            
+
     public File getConfigFile() {
         return configFile;
     }
-            
+
     public File getJavaVersionDirectory() {
         return javaVersionDirectory;
     }
-    
+
     public File getOutputFileDirectory() {
         return outputFileDirectory;
     }
-    
+
     public File getRootDirectory() {
         return rootDirectory;
-    }
-    
-    public String getRunMode() {
-        return runMode;
     }
 
     public File getSourceFileDirectory() {
@@ -47,64 +42,58 @@ public class ApplicationSettings {
     public File getTestCaseDirectory() {
         return testCaseDirectory;
     }
-    
-    public boolean getDisplayOutputCheck(){
+
+    public boolean getDisplayOutputCheck() {
         return displayOutputCheck;
     }
 
     public File getSettingsFile() {
         return settingsFile;
     }
-    
+
     public void setConfigFile(File file) {
         configFile = file;
         ApplicationSettingsController.writeDataToSettingsFile(this);
     }
-    
+
     public void setJavaVersionDirectory(File directory) {
         javaVersionDirectory = directory;
         ApplicationSettingsController.writeDataToSettingsFile(this);
     }
-    
+
     public void setOutputFileDirectory(File directory) {
         outputFileDirectory = directory;
         ApplicationSettingsController.writeDataToSettingsFile(this);
     }
-    
+
     public void setRootDirectory(File directory) {
         this.rootDirectory = directory;
-        ApplicationSettingsController.writeDataToSettingsFile(this);
-    }
-    
-    public void setRunMode(String runMode) {
-        this.runMode = runMode;
         ApplicationSettingsController.writeDataToSettingsFile(this);
     }
 
     public void setSourceFileDirectory(File directory) {
         sourceFileDirectory = directory;
-	ApplicationSettingsController.writeDataToSettingsFile(this);
+        ApplicationSettingsController.writeDataToSettingsFile(this);
     }
 
     public void setTestCaseDirectory(File directory) {
         testCaseDirectory = directory;
         ApplicationSettingsController.writeDataToSettingsFile(this);
     }
-    
-    public void setDisplayOutputCheck(boolean value){
+
+    public void setDisplayOutputCheck(boolean value) {
         displayOutputCheck = value;
         ApplicationSettingsController.writeDataToSettingsFile(this);
     }
-    
+
     @Override
     public String toString() {
         String string = "Settings: \n\n";
-        
+
         string += "Config file: " + configFile.getAbsolutePath() + "\n";
         string += "Java directory: " + javaVersionDirectory.getAbsolutePath() + "\n";
         string += "Output file: " + outputFileDirectory.getAbsolutePath() + "\n";
         string += "Root directory: " + rootDirectory.getAbsolutePath() + "\n";
-        string += "Run mode: " + runMode + "\n";
         string += "Source file: " + sourceFileDirectory.getAbsolutePath() + "\n";
         string += "Test directory: " + testCaseDirectory.getAbsolutePath() + "\n";
         string += "Display Output: " + displayOutputCheck + "\n";
