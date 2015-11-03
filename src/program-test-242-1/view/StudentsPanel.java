90f62aa3eee3fc6f7a6a207e164b614e50595be9
Ryan Yosua <rcy5017@psu.edu>
2015-11-03 12:33:11 -0500

    Started adding students to the student panel.

--- src/program-test-242-1/view/StudentsPanel.java
+++ src/program-test-242-1/view/StudentsPanel.java
@@ -1,8 +1,13 @@
 package view;
 
+import controller.BatchConfigReader;
+import java.util.ArrayList;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import model.ApplicationSettings;
+import model.Results;
+import model.Student;
 
 public class StudentsPanel extends JPanel {
     
@@ -12,6 +17,25 @@ public class StudentsPanel extends JPanel {
     public StudentsPanel(StudentsFrame frame, ApplicationSettings settings) {
         this.frame = frame;
         this.settings = settings;
+        
+        createComponents();
+    }
+    
+    private void createComponents() {
+        JCheckBox selectAllCheckBox = new JCheckBox("Select All");
+        this.add(selectAllCheckBox);
+        
+        // TEMPORARY - We need to refactor this class, or get an instance from BatchTester.
+        Results results = new Results();
+            
+        // Read students from config.
+        BatchConfigReader studentReader = new BatchConfigReader(results, settings);
+        ArrayList<Student> students = studentReader.readStudentsFromConfig();
+        
+        for (Student student: students) {
+            JCheckBox studentCheckbox = new JCheckBox(student.getStudentPath());
+            this.add(studentCheckbox);
+        }
     }
     
 }
