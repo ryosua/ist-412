9a29e1e0c768b48de3d2c9969176d6e7b9402fdb
Ryan Yosua <rcy5017@psu.edu>
2015-11-10 17:54:44 -0500

    Removed auto generated code.

--- src/program-test-242-1/view/OutputFrame.java
+++ src/program-test-242-1/view/OutputFrame.java
@@ -1,24 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
 import javax.swing.JFrame;
 import model.ApplicationSettings;
 
-/**
- *
- * @author dos5385
- */
 public class OutputFrame extends JFrame {
-    
+
     private final String results;
     private final ApplicationSettings settings;
-            
-    private OutputPanel output;
 
+    private OutputPanel output;
 
     public OutputFrame(String results, ApplicationSettings settings) {
         this.results = results;
