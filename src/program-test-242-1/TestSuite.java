658843b98a26eeccdd0dd80aee1024513d987ce0
Ryan Yosua <rcy5017@psu.edu>
2015-11-11 14:13:48 -0500

    Added tests for main.

--- src/program-test-242-1/TestSuite.java
+++ src/program-test-242-1/TestSuite.java
@@ -4,10 +4,12 @@ import test.BatchTesterTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import test.ApplicationSettingsTest;
+import test.MainTest;
 import test.StudentReaderTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+    MainTest.class,
     ApplicationSettingsTest.class,
     FileFilterTest.class,
     ResultsControllerTest.class,
