034b8517410d3cf1a22e328660edcb8e78a2e466
Dan Servich <dservich@gmail.com>
2015-11-10 19:03:20 -0500

    student Output Table for listing

---
+++ src/program-test-242-1/model/StudentTableModel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Dan
+ */
+public class StudentTableModel extends AbstractTableModel {
+
+    ArrayList<Student> studentArrayList;
+    String[] columnHeaders = {"Students"};
+    public StudentTableModel(ArrayList<Student> students){
+        studentArrayList = students;
+    }
+    
+    
+    
+    @Override
+    public int getRowCount() {
+        return studentArrayList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 1;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return studentArrayList.get(rowIndex).getStudentName();
+    }
+    
+}
--- src/program-test-242-1/view/StudentOutputPanel.java
+++ src/program-test-242-1/view/StudentOutputPanel.java
@@ -4,7 +4,10 @@ import java.awt.BorderLayout;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import model.ApplicationSettings;
+import model.StudentTableModel;
 import view.listener.CloseOutputWindowListener;
 import view.listener.OpenOutputWindowListener;
 import view.listener.SourceOutputWindowListener;
@@ -38,6 +41,12 @@ public class StudentOutputPanel extends JPanel {
         sourceButton = new JButton("Open Source");
         sourceButton.addActionListener(new SourceOutputWindowListener(outputPanel, settings));
         
+        JScrollPane theStudentScrollPane = new JScrollPane();
+        
+        JTable studentTable = new JTable();
+        studentTable.setModel(new StudentTableModel(settings.getStudents()));
+        
+        
         JPanel buttonPanel = new JPanel();
         BoxLayout layout = new BoxLayout(buttonPanel, BoxLayout.Y_AXIS);
         buttonPanel.setLayout(layout);
@@ -46,6 +55,7 @@ public class StudentOutputPanel extends JPanel {
         buttonPanel.add(sourceButton);
         buttonPanel.add(closeButton);
         
+        this.add(theStudentScrollPane, BorderLayout.CENTER);
         this.add(buttonPanel, BorderLayout.SOUTH);
     }
 }
